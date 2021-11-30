c925d9fcb1e3489b1298146870473b1c8f43ef8a
Jia-A <agrawaljiya2011@gmail.com>
2021-12-01 01:03:30 +0530

    refractor

--- extra1.js
+++ extra1.js
@@ -5,5 +5,5 @@ var spl = str.split(",");
 var rev1=spl[0].split(" ").reverse().join(" ")
 var rev2=spl[1].split(" ").reverse().join(" ")
 
-var final = rev1.concat(",").concat(rev2)
+var final = rev1 +" , " +rev2;
 console.log(final);
